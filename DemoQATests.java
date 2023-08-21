612d5cc1af26a2977fa2f76f42da7bf793e784d3
Elena Zaytseva <139309253+HelenZA4@users.noreply.github.com>
2023-08-21 22:50:23 +0700

    Update DemoQATests.java

--- DemoQATests.java
+++ DemoQATests.java
@@ -34,159 +34,62 @@ public class DemoQATests {
             driver.get("https://demoqa.com/automation-practice-form");
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        }
     }
+
     @Test
-    public void inputFirstNameTest() throws InterruptedException {
+    public void fillOutPracticeFormTest() throws InterruptedException {
         driver.get("https://demoqa.com/automation-practice-form");
 
         WebElement firstNameInput = driver.findElement(By.id("firstName"));
         firstNameInput.sendKeys("Elena");
-        Thread.sleep(2000);
-
-        firstNameInput.clear();
-        Thread.sleep(2000);
-
-        Assertions.assertTrue(firstNameInput.getText().equals(""));
-    }
-
-    @Test
-    public void inputLastNameTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-
+        Assertions.assertEquals("Elena", driver.findElement(By.id("firstName")).getAttribute("value"), "Корректное значение в поле 'Имя'");
+        
         WebElement lastNameInput = driver.findElement(By.id("lastName"));
         lastNameInput.sendKeys("Zaytseva");
-        Thread.sleep(2000);
-
-        lastNameInput.clear();
-        Thread.sleep(2000);
-
-        Assertions.assertTrue(lastNameInput.getText().equals(""));
-    }
-
-    @Test
-    public void inputEmailTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        Assertions.assertEquals("Zaytseva", driver.findElement(By.id("lastName")).getAttribute("value"), "Корректное значение в поле 'Фамилия'");
 
         WebElement emailInput = driver.findElement(By.id("userEmail"));
         emailInput.sendKeys("elena@example.com");
-        Thread.sleep(2000);
+        Assertions.assertEquals("elena@example.com", driver.findElement(By.id("userEmail")).getAttribute("value"), "Корректное значение в поле 'Email'");
 
-        emailInput.clear();
-        Thread.sleep(2000);
-
-        Assertions.assertTrue(emailInput.getText().equals(""));
-    }
-
-    @Test
-    public void selectGenderTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-
-        driver.findElement(By.xpath("//*[@id='gender-radio-2']")).isSelected();
-        Thread.sleep(3000);
-    }
-
-    @Test
-    public void inputNumberTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        WebElement genderSelect = driver.findElement(By.xpath("//*[@id='gender-radio-2']"));
+        genderSelect.isSelected();
 
         WebElement input = driver.findElement(By.cssSelector("input[id='userNumber']"));
         input.sendKeys("1234567890");
-        Thread.sleep(2000);
 
-        input.clear();
-        Thread.sleep(2000);
+        WebElement dateOfBirthInput = driver.findElement(By.xpath("//div[@class='react-datepicker__input-container']"));
+        dateOfBirthInput.click();
 
-        Assertions.assertTrue(input.getText().equals(""));
-    }
+        WebElement monthDropdown = driver.findElement(By.xpath("//select[@class='react-datepicker__month-select']"));
+        monthDropdown.click();
+        driver.findElement(By.xpath("//option[contains(text(), 'April')]"));
 
-    @Test
-    public void selectBirthDateTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        WebElement yearDropdown = driver.findElement(By.xpath("//select[@class='react-datepicker__year-select']"));
+        yearDropdown.click();
+        driver.findElement(By.xpath("//option[contains(text(), '1990')]"));
 
-        WebElement input = driver.findElement(By.id("dateOfBirthInput"));
-        input.click();
-
-        WebElement monthDropdown = driver.findElement(By.xpath("//div//select[@class='react-datepicker__month-select']"));
-        Select monthSelect = new Select(monthDropdown);
-        monthSelect.selectByVisibleText("April");
-
-        WebElement yearDropdown = driver.findElement(By.xpath("//div//select[@class='react-datepicker__year-select']"));
-        Select yearSelect = new Select(yearDropdown);
-        yearSelect.selectByVisibleText("1990");
-
-        WebElement daySelect = driver.findElement(By.xpath("//div[@aria-label='Choose Sunday, April 1st, 1990']"));
+        WebElement daySelect = driver.findElement(By.xpath("//div[@class='react-datepicker__month']/div[1]/div[1]"));
         daySelect.click();
-        Thread.sleep(2000);
-    }
 
-    @Test
-    public void inputSubjectsTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        WebElement subjectsInput = driver.findElement(By.id("subjectsInput"));
+        subjectsInput.sendKeys("Economics, English");
 
-        WebElement input = driver.findElement(By.id("subjectsInput"));
-        input.sendKeys("Economics");
-        input.sendKeys("English");
-        Thread.sleep(2000);
-
-        input.clear();
-        Thread.sleep(2000);
-
-        Assertions.assertTrue(input.getText().equals(""));
-    }
-
-    @Test
-    public void selectCheckboxTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-        WebElement hobbiesCheckbox = driver.findElement(By.xpath("//label[@for='hobbies-checkbox-2']"));
+        WebElement hobbiesCheckbox = driver.findElement(By.id("hobbies-checkbox-2"));
         hobbiesCheckbox.click();
-        Thread.sleep(3000);
-    }
 
-    @Test
-    public void uploadPictureTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        WebElement addressInput = driver.findElement(By.id("currentAddress"));
+        addressInput.sendKeys("123, Street, City");
 
-        WebElement input = driver.findElement(By.xpath("//*[@id='uploadPicture']"));
-        String text = input.getText();
-        Thread.sleep(3000);
-    }
+        WebElement stateDropdown = driver.findElement(By.xpath("//div[@id='stateCity-wrapper']/div[2]"));
+        stateDropdown.click();
+        driver.findElement(By.xpath("//div[contains(text(), 'NCR')]"));
 
-    @Test
-    public void inputAddressTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-
-        WebElement currentAddressInput = driver.findElement(By.xpath("//*[@id='currentAddress']"));
-        currentAddressInput.sendKeys("123, Street, City");
-        Thread.sleep(2000);
-
-        currentAddressInput.clear();
-        Thread.sleep(2000);
-
-        Assertions.assertTrue(currentAddressInput.getText().equals(""));
-    }
-
-    @Test
-    public void selectStateTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-
-       driver.findElement(By.xpath("//div[@id='stateCity-wrapper']/div[2]")).isDisplayed();
-        Thread.sleep(2000);
-    }
-
-    @Test
-    public void selectCityTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-
-        driver.findElement(By.xpath("//div[@id='stateCity-wrapper']/div[3]")).isEnabled();
-        Thread.sleep(2000);
-    }
-
-    @Test
-    public void clickSubmitTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
-
-        driver.findElement(By.xpath("//*[@id='submit']")).click();
+        WebElement cityDropdown = driver.findElement(By.xpath("//div[@id='stateCity-wrapper']/div[3]"));
+        cityDropdown.isEnabled();
+        
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
         Thread.sleep(3000);
     }
 
